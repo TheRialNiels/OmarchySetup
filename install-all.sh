67e3427dc5083c9c86a7a07b91b193e53703ee37
Jonathan Dominguez <jonathan24.dp@gmail.com>
2026-01-14 22:15:30 -0600

    Remove opencode script

--- install-all.sh
+++ install-all.sh
@@ -8,13 +8,12 @@
 ./install-vial.sh
 ./install-gemini-cli.sh
 ./install-zellij.sh
-./install-opencode.sh
 ./install-dotfiles.sh
 ./install-hyprland-overrides.sh
 #./install-hyprsunset-overrides.sh # Not working
 
 ./set-omarchy-configs.sh
-./set-keyboard.sh
+# ./set-keyboard.sh
 ./remove-webapps.sh
 ./remove-packages.sh
 ./set-zsh.sh
--- install-opencode.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Install opencode-bin
-yay -S --noconfirm --needed opencode-bin
