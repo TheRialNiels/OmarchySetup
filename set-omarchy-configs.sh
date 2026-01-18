5508020bcdb52853c498cf9ba7648897a93a4b43
Jonathan Dominguez <jonathan24.dp@gmail.com>
2026-01-18 11:53:13 -0600

    enable suspend

--- set-omarchy-configs.sh
+++ set-omarchy-configs.sh
@@ -2,3 +2,10 @@
 
 omarchy-font-set "JetBrainsMono Nerd Font Mono"
 omarchy-theme-set Nord
+
+STATE_FILE=~/.local/state/omarchy/toggles/suspend-on
+
+if [[ ! -f $STATE_FILE ]]; then
+  # Enable
+  omarchy-toggle-suspend
+fi
